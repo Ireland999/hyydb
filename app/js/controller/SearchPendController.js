d283408f8dae0ac28b2af3b9bb4e1928ec69e15b
ireland999 <948450161@qq.com>
2015-03-27 18:18:44 +0800

    2015-03-27

--- app/js/controller/SearchPendController.js
+++ app/js/controller/SearchPendController.js
@@ -43,7 +43,40 @@
       }
       
     };
+    var ConfigFun=function(){
+      wx.config({
+          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId: 'wx7e982df0ff12914e', // 必填，公众号的唯一标识
+          timestamp:1427440956 , // 必填，生成签名的时间戳
+          nonceStr: '53ab4296-f6f2-4907-b822-00095296488f', // 必填，生成签名的随机串
+          signature: '0a7c31712b7bbb3de92beceeae2589c9ab218e88',// 必填，签名，见附录1
+          jsApiList: ['scanQRCode'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+      });
+      wx.ready(function(){
+        console.log(111);
+          // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+          Scan();
+      });
+      wx.error(function(res){
+        alert(res.errMsg);
+
+      });
+    };
+    var Scan=function(){
+      console.log('调用微信扫一扫接口');
+      alert('nihao');
+      wx.scanQRCode({
+          needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+          scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+          success: function (res) {
+          var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+          console.log(result);
+          console.log('调用成功');
+      }
+      });
+    };
     $scope.SearchPendLoad=SearchPendLoad;
     $scope.ShowAll=ShowAll;
+    $scope.ConfigFun=ConfigFun;
   }
 })();
--- app/router/app.urgent.js
+++ app/router/app.urgent.js
@@ -1,8 +1,8 @@
 (function(){
   function config($stateProvider,$urlRouterProvider){
-    // $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.otherwise("/");
     $stateProvider.state('SearchList',{
-      url:'/',
+      url:'/ScanInfo',
       templateUrl:'views/admin/SearchList.html'
     }).state('SearchList.SearchResolved',{
       url:'SearchResolved',
@@ -13,8 +13,11 @@
     }).state('Comproducts',{
       url:'Comproducts/{DrugstoreName}',
       templateUrl:'views/admin/Comproducts.html'
+    }).state('ScanInfo',{
+      url:'/',
+      templateUrl:'views/admin/ScanInfo.html'
     });
   }
   config.$inject = ['$stateProvider','$urlRouterProvider'];
-  angular.module('app',['ui.router']).config(config);
+  angular.module('app',['ui.router','toggle-switch']).config(config);
 })();
